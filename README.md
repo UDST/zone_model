5e398f5b483b7a1c4ed9630c794cd5c913d53f8c
Eddie Janowicz <eddie.janowicz@gmail.com>
2017-03-28 12:48:59 -0700

    cleanup

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Template for zonal UrbanSim model.
 
 
+Data (model_data.h5) for the sandiego_zone region:  
+
+https://storage.googleapis.com/urbansim/zone_model/model_data.h5
--- zone_model/datasources.py
+++ zone_model/datasources.py
@@ -1,14 +1,13 @@
 import os
-import yaml
-import numpy as np
 import pandas as pd
 
 import orca
 from urbansim.utils import misc
-from urbansim.utils import yamlio
+from urbansim.models import MNLDiscreteChoiceModel
+
+import utils
 
 
-# Data store
 @orca.injectable('store', cache=True)
 def hdfstore(settings):
     return pd.HDFStore(
@@ -16,56 +15,32 @@ def hdfstore(settings):
         mode='r')
 
 
-# Generator function for configs
-def register_config_injectable_from_yaml(injectable_name, yaml_file):
-    """
-    Create orca function for YAML-based config injectables.
-    """
-    @orca.injectable(injectable_name, cache=True)
-    def func():
-        with open(os.path.join(misc.configs_dir(), yaml_file)) as f:
-            config = yaml.load(f)
-            return config
-    return func
-
 # Configs
-for yaml_file in ["settings.yaml", "model_structure.yaml", "neighborhood_vars.yaml", "yaml_configs.yaml"]:
+for yaml_file in ["settings.yaml", "model_structure.yaml", "yaml_configs.yaml"]:
     if os.path.exists('./configs/' + yaml_file):
         injectable_name = yaml_file.split('.')[0]
-        register_config_injectable_from_yaml(injectable_name, yaml_file)
+        utils.register_config_injectable_from_yaml(injectable_name, yaml_file)
 
-# Model structure
-ms = orca.get_injectable('model_structure')
 
 # Geographic level of model
-template = ms['template']
-geography = ms['geography']
-geography_id = ms['geography_id']
+model_structure = orca.get_injectable('model_structure')
+template = model_structure['template']
+geography = model_structure['geography']
+geography_id = model_structure['geography_id']
 orca.add_injectable("template", template)
 orca.add_injectable("geography", geography)
 orca.add_injectable("geography_id", geography_id)
 print 'Model template is: %s. Operating on %s with %s.' % (template, geography, geography_id)
 
 
-# Generator functions
-
-def register_table_from_store(table_name):
-    """
-    Create orca function for tables from data store.
-    """
-    @orca.table(table_name, cache=True)
-    def func(store):
-        return store[table_name]
-    return func
-
 # Tables from data-store
 for table in [geography, 'jobs', 'households', 'residential_units', 'non_residential_units']:
-    register_table_from_store(table)
+    utils.register_table_from_store(table)
 
-# Other
+
 @orca.injectable('year')
 def year():
-    default_year = ms['base_year']
+    default_year = model_structure['base_year']
     try:
         iter_var = orca.get_injectable('iter_var')
         if iter_var is not None:
@@ -74,3 +49,7 @@ def year():
             return default_year
     except:
         return default_year
+
+
+# Change-sets
+orca.add_injectable("track_changes", False)
