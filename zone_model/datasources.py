3569e35aa15e85cd33bb09f5c91b7220fac056b8
Paul Sohn <pksohn@gmail.com>
2017-03-31 12:09:48 -0700

    PEP 8 changes for datasources.py and simulate.py

--- zone_model/datasources.py
+++ zone_model/datasources.py
@@ -15,11 +15,11 @@ def hdfstore(settings):
 
 
 # Configs
-for yaml_file in ["settings.yaml", "model_structure.yaml", "yaml_configs.yaml"]:
+for yaml_file in ["settings.yaml", "model_structure.yaml",
+                  "yaml_configs.yaml"]:
     if os.path.exists('./configs/' + yaml_file):
         injectable_name = yaml_file.split('.')[0]
         utils.register_config_injectable_from_yaml(injectable_name, yaml_file)
-
 
 # Geographic level of model
 model_structure = orca.get_injectable('model_structure')
@@ -29,11 +29,12 @@ geography_id = model_structure['geography_id']
 orca.add_injectable("template", template)
 orca.add_injectable("geography", geography)
 orca.add_injectable("geography_id", geography_id)
-print 'Model template is: %s. Operating on %s with %s.' % (template, geography, geography_id)
-
+print 'Model template is: %s. Operating on %s with %s.' % (
+    template, geography, geography_id)
 
 # Tables from data-store
-for table in [geography, 'jobs', 'households', 'residential_units', 'non_residential_units']:
+for table in [geography, 'jobs', 'households', 'residential_units',
+              'non_residential_units']:
     utils.register_table_from_store(table)
 
 
@@ -46,15 +47,15 @@ def year():
             return iter_var
         else:
             return default_year
-    except:
+    except KeyError:
         return default_year
 
 
 # Change-sets
 orca.add_injectable("track_changes", False)
 
-
-# Set up location choice model objects. Register as injectable to be used throughout simulation
+# Set up location choice model objects.
+# Register as injectable to be used throughout simulation
 location_choice_models = {}
 model_configs = utils.get_model_category_configs()
 for model_category_name, model_category_attributes in model_configs.items():
@@ -62,7 +63,8 @@ for model_category_name, model_category_attributes in model_configs.items():
         model_config_files = model_category_attributes['config_filenames']
 
         for model_config in model_config_files:
-            model = utils.create_lcm_from_config(model_config, model_category_attributes)
+            model = utils.create_lcm_from_config(model_config,
+                                                 model_category_attributes)
             location_choice_models[model.name] = model
 
 orca.add_injectable('location_choice_models', location_choice_models)
--- zone_model/simulate.py
+++ zone_model/simulate.py
@@ -4,11 +4,14 @@ import datasources
 import variables
 import models
 
-transition_models = ['simple_jobs_transition', 
-                     'simple_households_transition', 
-                     'simple_residential_units_transition', 
+transition_models = ['simple_jobs_transition',
+                     'simple_households_transition',
+                     'simple_residential_units_transition',
                      'simple_non_residential_units_transition']
 
-choice_models = ['elcm1', 'hlcm1', 'hlcm2', 'hlcm3', 'hlcm4', 'rdplcm1', 'nrdplcm1']
+choice_models = ['elcm1',
+                 'hlcm1', 'hlcm2', 'hlcm3', 'hlcm4',
+                 'rdplcm1',
+                 'nrdplcm1']
 
 orca.run(transition_models + choice_models)
