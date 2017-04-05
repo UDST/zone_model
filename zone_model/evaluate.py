f6f38303fb02f2a76d6ce1cf611576368de6cb0a
Eddie Janowicz <eddie.janowicz@gmail.com>
2017-04-04 18:07:14 -0700

    add score method to SimulationChoiceModel

---
+++ zone_model/evaluate.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+import orca
+
+from zone_model import datasources
+from zone_model import variables
+from zone_model import models
+from zone_model import utils
+
+
+def correlate(observed, predicted):
+    return observed.corr(predicted)
+
+
+xref_url = 'https://storage.googleapis.com/urbansim/zone_model/zone_summary_xref.csv'  # noqa
+summary_geog_xref = pd.read_csv(xref_url)
+summary_geog_xref = summary_geog_xref.set_index('zone_id').summary_geog_id
+
+location_choice_models = orca.get_injectable('location_choice_models')
+for model_name, model in location_choice_models.items():
+    print("Evaluating model {}".format(model_name))
+    model.summary_alts_xref = summary_geog_xref
+
+    accuracy_score = model.score()
+    print("  Accuracy score is {}".format(accuracy_score))
+
+    rmse = model.score(scoring_function=mean_squared_error, aggregate=True)**.5
+    print("  RMSE is {}".format(rmse))
+
+    r2 = model.score(scoring_function=r2_score, aggregate=True)
+    print("  R2 is {}".format(r2))
+
+    corr = model.score(scoring_function=correlate, aggregate=True)
+    print("  Correlation is {}".format(corr))
